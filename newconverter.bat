872989bf9724358283138213616a77aa557dce83
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2023-08-09 16:02:08 +0200

    finaly add working .bat

--- newconverter.bat
+++ newconverter.bat
@@ -1,28 +1,33 @@
 @echo off
 setlocal enabledelayedexpansion
 
-rem Określ ścieżkę do katalogu skryptu
-set "scriptDir=%~dp0"
+rem Ścieżka do folderu, który ma zostać przeszukany wraz z podfolderami
+set "folderPath=%~dp0"
 
-rem Utwórz plik tekstowy z danymi zgodnie z formułą
-(
-    echo if (SERVER^) then
-    echo   -- EXAMPLE
-    rem Pętla przetwarzająca przeciągnięte foldery/pliki
-    for %%I in (%*) do (
-        rem Sprawdzamy, czy przeciągnięty element to plik
-        if exist "%%~I" (
-            rem Pobieramy nazwę pliku (bez ścieżki i rozszerzenia)
-            set "fileName=%%~nxI"
-            rem Dodajemy wpis do pliku tekstowego dla nieskompresowanego pliku
-            echo   resource.AddFile("%%~I") -- Uncompressed >> "%scriptDir%resources.txt"
-            rem Dodajemy wpis do pliku tekstowego dla skompresowanego pliku (o ile jeszcze nie jest skompresowany)
-            echo   resource.AddFile("%%~dpnI.bz2") -- Compressed ( smaller filesize ) >> "%scriptDir%resources.txt"
-        )
-    )
-    echo end
-) > "%scriptDir%resources.txt"
+rem Ścieżka do pliku zasobów
+set "outputFile=%folderPath%resources.txt"
 
-echo Plik tekstowy "resources.txt" został utworzony w katalogu skryptu .bat.
+rem Tworzymy nowy plik resources.txt lub nadpisujemy istniejący
+echo. > "%outputFile%"
+
+rem Funkcja rekurencyjna przetwarzająca foldery i pliki
+:process_folder
+for /r "%folderPath%" %%F in (*.bz2) do (
+    rem Pobieramy pełną ścieżkę do pliku .bz2
+    set "filePath=%%~fF"
+    rem Usuwamy początkową część ścieżki, aby pozostała tylko względna ścieżka od folderPath
+    set "relativePath=!filePath:%folderPath%=!"
+    rem Dodajemy linie zasobu do pliku tekstowego
+    echo resource.AddFile("!relativePath!") >> "%outputFile%"
+)
+
+rem Wywołujemy funkcję rekurencyjnie dla podfolderów
+for /d %%D in ("%folderPath%*") do (
+    set "folderPath=%%D"
+    call :process_folder
+)
+
+goto :eof
+
+echo Plik resources.txt został wygenerowany.
 pause
-
